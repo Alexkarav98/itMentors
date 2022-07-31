fe385e3797bb3a5897d0ab395d0da49612845d17
TheRenat <therenat@mail.ru>
2022-07-31 21:22:14 +0300

    add VC and some logic for it

---
+++ itMentors/ViewControllers/MentorTabBarViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MentorTabBarViewController.swift
+//  itMentors
+//
+//  Created by Renat Ibragimov on 31.07.2022.
+//
+
+import UIKit
+
+class MentorTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //setupViewControllers()
+
+    }
+    
+    // передача данных на экраны
+    /*
+    private func setupViewControllers() {
+        guard let mentorInfoVC = viewControllers?.first as? MentorInfoViewController else { return }
+        guard let reviewVC = viewControllers?.last as? ReviewViewController else { return }
+        
+        mentorInfoVC.mentor = mentor
+        reviewVC.mentor = mentor
+    }
+     */
+}
